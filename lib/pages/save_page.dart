b7e580c22e31ad488afd8ad5a5d0853618e4e003
Void-CA <castilloari282@gmail.com>
2025-02-09 02:14:46 -0600

    Adding sqflite

--- lib/pages/save_page.dart
+++ lib/pages/save_page.dart
@@ -16,14 +16,25 @@ class SavePage extends StatelessWidget {
 }
 
 class _FormSave extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(20),
       child: Form(
+        key: _formKey,
         child: Column(
           children: <Widget>[
             TextFormField(
+              controller: titleController,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'El contenido no puede estar vacio';
+                }
+                return null;
+              },
               decoration: InputDecoration(
                 labelText: 'Titulo',
                 border: OutlineInputBorder(),
@@ -33,10 +44,31 @@ class _FormSave extends StatelessWidget {
               height: 10,
             ),
             TextFormField(
+              maxLines: 8,
+              maxLength: 1000,
+              controller: contentController,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'El contenido no puede estar vacio';
+                }
+                return null;
+              },
               decoration: InputDecoration(
                 labelText: 'Contenido',
                 border: OutlineInputBorder(),
               ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('Guardando...')));
+                }
+              },
+              child: Text('Guardar'),
             ),
           ],
         ),
